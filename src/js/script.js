97f2435cebfb93405709a187cd41fc0f7d0023a2
roge5716 <roger.avalos@rackspace.com>
2018-09-24 11:47:01 -0500

    finishing 3rd step wip cleanu[

--- src/js/script.js
+++ src/js/script.js
@@ -8,22 +8,36 @@
       validate: false,
     };
     const options = $.extend({}, defaults, opts);
-    const $cost = $('.rsSignup-cost');
+    const $cost = $('.rsSignup-cost, #summary-total');
     const $boxes = $('.rsSignup-boxes');
     const $globalErrors = $('.rsForm-global-errors');
+    const $loader = $('.rsForm-loader');
+    const $submitStep = $('.rsForm-submitStep');
+    const $productSummary = $('.rsForm-summary-products');
     let totalCost = 0;
     let totalQuantity = 0;
+    const products = {
+      email: 0,
+      emailPlus: 0,
+      exchange: 0,
+    };
 
     const methods = {
       addBoxes($step) {
         totalQuantity = 0;
         totalCost = 0;
+        products.email = 0;
+        products.emailPlus = 0;
+        products.exchange = 0;
         $step.find('[name="rs-add-quantity"]').each(function input() {
-          const pennies = parseInt($(this).attr('data-cost'), 10);
-          const val = parseInt($(this).val(), 10);
+          const $inp = $(this);
+          const type = $inp.attr('data-product');
+          const pennies = parseInt($inp.attr('data-cost'), 10);
+          const val = parseInt($inp.val(), 10);
           if (val >= 0) {
             totalCost += (pennies * val);
             totalQuantity += val;
+            products[type] = val;
           }
         });
         if (totalQuantity >= 0) {
@@ -163,6 +177,40 @@
         $(this).on('keyup', () => methods.addBoxes($this));
       });
 
+      const moveNext = () => {
+        // swap to next step
+        $this.css('height', $this.outerHeight());
+        $this.css('width', $this.outerWidth());
+        $this.addClass('position-absolute');
+        $this.animate({
+          marginLeft: '-200px',
+          opacity: 0,
+        }, 300);
+        $this.next().addClass('rsMultiStep-stepActive');
+        setTimeout(() => {
+          $this.removeClass('rsMultiStep-stepActive');
+        }, 300);
+        $msProgress.find(`.rsMultiStep-progressStep${indexNum}`)
+                  .next().find('.rsMultiStep-progressStep-num')
+                  .addClass('rsMultiStep-progressStep-numActive');
+      };
+
+      const moveBack = () => {
+        $this.removeClass('rsMultiStep-stepActive');
+        $this.prev().removeClass('position-absolute');
+        $this.prev().addClass('rsMultiStep-stepActive');
+        $this.prev().animate({
+          marginLeft: '0px',
+          opacity: 1,
+        }, 300);
+        setTimeout(() => {
+          $this.prev().css('height', '');
+          $this.prev().css('width', '');
+        }, 300);
+        $msProgress.find(`.rsMultiStep-progressStep${indexNum} .rsMultiStep-progressStep-num`)
+                  .removeClass('rsMultiStep-progressStep-numActive');
+      };
+
       // next click event
       $this.find('.rsMultiStep-nextBtn, .rsMultiStep-submitBtn').click((e) => {
         if (!$this.is(':animated')) {
@@ -180,23 +228,37 @@
             return;
           }
 
-          // TODO check if submit button, then run loader and move to Confirmation
-
-          // swap to next step
-          $this.css('height', $this.outerHeight());
-          $this.css('width', $this.outerWidth());
-          $this.addClass('position-absolute');
-          $this.animate({
-            marginLeft: '-200px',
-            opacity: 0,
-          }, 300);
-          $this.next().addClass('rsMultiStep-stepActive');
-          setTimeout(() => {
-            $this.removeClass('rsMultiStep-stepActive');
-          }, 300);
-          $msProgress.find(`.rsMultiStep-progressStep${indexNum}`)
-                    .next().find('.rsMultiStep-progressStep-num')
-                    .addClass('rsMultiStep-progressStep-numActive');
+          // if clicking submit button, run loader, submit form and move to confirmation
+          if ($(e.currentTarget).hasClass('rsMultiStep-submitBtn')) {
+            const testCORS = 'https://cors-anywhere.herokuapp.com/';
+            $loader.show();
+            $submitStep.hide();
+            $.ajax({
+              type: 'POST',
+              url: `${testCORS}https://postman-echo.com/post`,
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              data: JSON.stringify({ form: 'data-here' }),
+              success() {
+                $productSummary.html(`
+                  <span class="rsForm-summary-quantity">Rackspace Email x ${products.email} Mailboxe(s)</span>
+                  <span class="rsForm-summary-quantity">Rackspace Email Plus x ${products.emailPlus} Mailboxe(s)</span>
+                  <span class="rsForm-summary-quantity">Microsoft Exchange x ${products.exchange} Mailboxe(s)</span>
+                `);
+                moveNext();
+              },
+              error() {
+                $globalErrors.show().html('There was an error. Please try again!');
+              },
+              complete() {
+                $loader.hide();
+                $submitStep.show();
+              },
+            });
+          } else {
+            moveNext();
+          }
         }
       });
 
@@ -204,19 +266,7 @@
       $this.find('.rsMultiStep-backBtn').click((e) => {
         if (!$this.prev().is(':animated')) {
           e.preventDefault();
-          $this.removeClass('rsMultiStep-stepActive');
-          $this.prev().removeClass('position-absolute');
-          $this.prev().addClass('rsMultiStep-stepActive');
-          $this.prev().animate({
-            marginLeft: '0px',
-            opacity: 1,
-          }, 300);
-          setTimeout(() => {
-            $this.prev().css('height', '');
-            $this.prev().css('width', '');
-          }, 300);
-          $msProgress.find(`.rsMultiStep-progressStep${indexNum} .rsMultiStep-progressStep-num`)
-                    .removeClass('rsMultiStep-progressStep-numActive');
+          moveBack();
         }
       });
     });
